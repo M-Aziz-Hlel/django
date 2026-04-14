b4320607726916b26daecdad7d21409a6fc47454
Tigana <m.aziz.hlel@gmail.com>
2026-04-14 18:33:45 +0100

    making user model

--- users/models.py
+++ users/models.py
@@ -6,5 +6,8 @@ from django.db import models
 class User(AbstractUser):
     email = models.EmailField(unique=True)
 
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
     def __str__(self):
         return self.username
